7701d970b55b073ff8788b1918ee12c2f9ec3da6
Anshul Upadhyay <upadhyay041298@gmail.com>
2026-03-13 22:45:35 +0530

    updated code

--- database.py
+++ database.py
@@ -1,18 +1,11 @@
 import os
-from dotenv import load_dotenv
 import mysql.connector
 
-load_dotenv()  # load .env file
-
-DB_HOST = os.getenv("DB_HOST")
-DB_USER = os.getenv("DB_USER")
-DB_PASSWORD = os.getenv("DB_PASSWORD")
-DB_NAME = os.getenv("DB_NAME")
-
-# Create a single DB connection to reuse
-db = mysql.connector.connect(
-    host=DB_HOST,
-    user=DB_USER,
-    password=DB_PASSWORD,
-    database=DB_NAME
-)
+def get_db():
+    return mysql.connector.connect(
+        host=os.getenv("DB_HOST"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        database=os.getenv("DB_NAME"),
+        port=4000
+    )
