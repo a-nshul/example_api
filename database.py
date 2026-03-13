049b60607bef957997f63eec088a59c722e886b3
Marutipandey <pp874765@gmail.com>
2026-03-13 08:20:43 +0530

    Remove

--- database.py
+++ database.py
@@ -1,17 +1,17 @@
 import os
 from dotenv import load_dotenv
+import os
 import mysql.connector
 
-load_dotenv()  # .env load karega
-
-host = os.getenv("DB_HOST")
-user = os.getenv("DB_USER")
-password = os.getenv("DB_PASSWORD")
-database = os.getenv("DB_NAME")
+DB_HOST = os.getenv("DB_HOST")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_NAME = os.getenv("DB_NAME")
 
 conn = mysql.connector.connect(
-    host=host,
-    user=user,
-    password=password,
-    database=database
+    host=DB_HOST,
+    user=DB_USER,
+    password=DB_PASSWORD,
+    database=DB_NAME
 )
+
