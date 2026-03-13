344c2c6f6b65674de442e268130e6e4583ba97c7
Marutipandey <pp874765@gmail.com>
2026-03-13 07:53:12 +0530

    no

--- database.py
+++ database.py
@@ -1,10 +1,17 @@
+import os
+from dotenv import load_dotenv
 import mysql.connector
 
-db = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="Priya@1234",
-    database="hrms_db"
-)
+load_dotenv()  # .env load karega
 
-cursor = db.cursor(dictionary=True)
+host = os.getenv("DB_HOST")
+user = os.getenv("DB_USER")
+password = os.getenv("DB_PASSWORD")
+database = os.getenv("DB_NAME")
+
+conn = mysql.connector.connect(
+    host=host,
+    user=user,
+    password=password,
+    database=database
+)
